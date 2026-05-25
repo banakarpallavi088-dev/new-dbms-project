da4466f2f8df1482a273f2d9d1352e133cc92c22
Pallavi <banakarpallavi088@gmail.com>
2026-05-25 13:12:39 +0530

    Connected frontend to online backend

--- tourism-backend/server.js
+++ tourism-backend/server.js
@@ -1,23 +1,22 @@
 const express = require('express');
-const cors = require('cors');
-
+const cors    = require('cors');
 require('dotenv').config();
-require("./config/db");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
+
 app.use(express.json());
 
-app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/auth',     require('./routes/authRoutes'));
 app.use('/api/packages', require('./routes/packageRoutes'));
-app.use('/api/hotels', require('./routes/hotelRoutes'));
+app.use('/api/hotels',   require('./routes/hotelRoutes'));
 app.use('/api/bookings', require('./routes/bookingRoutes'));
 app.use('/api/payments', require('./routes/paymentRoutes'));
-app.use('/api/admin', require('./routes/adminRoutes'));
+app.use('/api/admin',    require('./routes/adminRoutes'));
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => 
-  console.log(`Server running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
